0dd81963904f3b0b5ec4a004ac293a692cb1d0ac
Sidd <siddheshposam20@gmail.com>
2024-03-23 14:42:58 +0530

    navbar

--- qtify2/src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Button.module.css";
-
-function Button({children}){
-    return <Button className={styles.button}>{children}</Button>;
-}
-
-export default Button;
--- qtify2/src/helpers/helpers.js
+++
@@ -1,6 +0,0 @@
-export function truncate(input, length = 5) {
-    if (input.length > length) {
-        return input.substring(0, length) + "...";
-    }
-    return input;
-}
