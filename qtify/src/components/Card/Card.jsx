3da302bac52eb216718e2e55c91ea9ffc6f869c9
Sidd <siddheshposam20@gmail.com>
2024-04-09 20:02:11 +0530

    modified songs section

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -2,6 +2,54 @@ import React from "react";
 import { Chip, Tooltip } from "@mui/material";
 import styles from "./Card.module.css";
 
+
+
+// function Card({ data, type}) {
+//   const getCard = (type) => {
+//     switch (type) {
+//       case "album": {
+//         const { image, follows, title, slug, songs} = data;
+//         return (
+//           <Tooltip title={`${songs.length} songs`} placement="top" arrow>
+//       <div className={styles.wrapper}>
+//         <div className={styles.card}>
+//           <img src={image} alt={title} className={styles.image} />
+//           <div className={styles.banner}>
+//             <Chip label={`${follows} Follows`} className={styles.chip} />
+//           </div>
+//         </div>
+//         <div className={styles.albumName}>
+//           <p>{title}</p>
+//         </div>
+//       </div>
+//     </Tooltip>
+//         )
+//       }
+//       case "song": {
+//         const { image, likes, title } = data;
+//         return (
+//           <div className={styles.wrapper}>
+//           <div className={styles.card}>
+//             <img src={image} alt="song" className={styles.image} />
+//             <div className={styles.banner}>
+//               <div className={styles.pill}>
+//                 <p>{likes} Likes</p>
+//               </div>
+//             </div>
+//           </div>
+//           <div className={styles.albumName}>
+//             <p>{title}</p>
+//           </div>
+//         </div>
+//         )
+//       }
+//       default:
+//         return <></>;
+//     }
+//   }
+//   return getCard(type);
+// }
+
 const Card = ({ album }) => {
   const songsCount = album && album.songs ? album.songs.length : 0;
   return (
@@ -22,3 +70,5 @@ const Card = ({ album }) => {
 };
 
 export default Card;
+
+
--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,18 +1,15 @@
-
-
-
-import React, { useState, useEffect } from 'react';
-import Carousel from '../Carousel/Carousel'; // Import the Carousel component
-import styles from './Section.module.css';
-import Card from '../Card/Card'; 
-import { Tabs, Tab } from '@mui/material'
+import React, { useState, useEffect } from "react";
+import Carousel from "../Carousel/Carousel"; // Import the Carousel component
+import styles from "./Section.module.css";
+import Card from "../Card/Card";
+import { Tabs, Tab } from "@mui/material";
 
 const Section = ({ title, fetchAlbums, fetchSongs, fetchFilters }) => {
   const [albums, setAlbums] = useState([]);
   const [collapsed, setCollapsed] = useState(true); // Start collapsed by default
   const [songs, setSongs] = useState([]);
   const [filters, setFilters] = useState([]);
-  const [selectedFilter, setSelectedFilter] = useState('All');
+  const [selectedFilter, setSelectedFilter] = useState("All");
 
   const toggleCollapse = () => {
     setCollapsed(!collapsed);
@@ -30,11 +27,11 @@ const Section = ({ title, fetchAlbums, fetchSongs, fetchFilters }) => {
   useEffect(() => {
     const Data = async () => {
       const genreData = await fetchFilters();
-      console.log(genreData)
+      console.log(genreData);
       if (Array.isArray(genreData)) {
-        setFilters(['All', ...genreData]);
+        setFilters(["All", ...genreData]);
       } else {
-        console.error('Genre data is not an array:', genreData);
+        console.error("Genre data is not an array:", genreData);
         // Handle the non-array response accordingly
       }
     };
@@ -54,36 +51,37 @@ const Section = ({ title, fetchAlbums, fetchSongs, fetchFilters }) => {
   const handleTabChange = (event, newValue) => {
     setSelectedFilter(newValue);
   };
-  
 
   return (
     <div className={styles.section}>
       <div className={styles.header}>
         <h2>{title}</h2>
         <button onClick={toggleCollapse}>
-          {collapsed ? 'Show All' : 'Collapse'}
+          {collapsed ? "Show All" : "Collapse"}
         </button>
       </div>
       <Tabs value={selectedFilter} onChange={handleTabChange}>
-      {filters.map(genre => (
-        <Tab key={genre} label={genre} value={genre} />
-      ))}
-    </Tabs>
+        {filters.map((genre) => (
+          <Tab key={genre} label={genre} value={genre} />
+        ))}
+      </Tabs>
       {collapsed ? (
         <>
-        
-        <div >
-          <Carousel albums={albums} />
-        </div>
-        {title === 'Songs' && (
-          <div>
-            <Carousel albums={songs} />
-          </div>
-        )}
+          {(title === "Top Albums" || title === "New Albums") && (
+            <div>
+              <Carousel albums={albums} />
+            </div>
+          )}
+
+          {title === "Songs" && (
+            <div>
+              <Carousel albums={songs} />
+            </div>
+          )}
         </>
       ) : (
         <div className={styles.grid}>
-          {albums.map(album => (
+          {albums.map((album) => (
             <Card key={album.id} album={album} />
           ))}
         </div>
@@ -93,4 +91,3 @@ const Section = ({ title, fetchAlbums, fetchSongs, fetchFilters }) => {
 };
 
 export default Section;
-
